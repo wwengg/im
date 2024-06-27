fd3f70db7122b3c922137ccd15f8136dc6832007
wwengg <wwwwangg@163.com>
2024-06-28 07:29:11 +0800

    fix show config

--- global/config.go
+++ global/config.go
@@ -18,23 +18,55 @@ type Config struct {
 }
 
 func (c *Config) Show() {
-	objVal := reflect.ValueOf(c).Elem()
-	objType := reflect.TypeOf(*c)
+	// objVal := reflect.ValueOf(c).Elem()
+	// objType := reflect.TypeOf(*c)
 
 	fmt.Println("===== IM Global Config =====")
-	for i := 0; i < objVal.NumField(); i++ {
-		objVal2 := reflect.ValueOf(objVal.Field(i)).Elem()
-		objType2 := reflect.TypeOf(objVal.Field(i))
+	// fmt.Printf("%+v\n", *c)
+	show(reflect.ValueOf(c).Elem(), reflect.TypeOf(*c))
+	// for i := 0; i < objVal.NumField(); i++ {
+	// 	field := objVal.Field(i)
+	// 	typeField := objType.Field(i)
 
-		for a := 0; a < objVal2.NumField(); a++ {
-			field2 := objVal2.Field(a)
-			typeField2 := objType2.Field(a)
-			fmt.Printf("%s: %v\n", typeField2.Name, field2.Interface())
+	// 	fmt.Printf("%s: %v\n", typeField.Name, field.Interface())
+	// 	// objVal2 := field
+	// 	// objType2 := field.Type()
+
+	// 	// for a := 0; a < objVal2.NumField(); a++ {
+	// 	// 	field2 := objVal2.Field(a)
+	// 	// 	typeField2 := objType2.Field(a)
+	// 	// 	fmt.Printf("%s: %v\n", typeField2.Name, field2.Interface())
+	// 	// }
+	// }
+	fmt.Println("==============================")
+}
+
+func show(objVal reflect.Value, objType reflect.Type) {
+
+	if objVal.Kind() == reflect.Slice {
+		objVal.Len()
+		for i := 0; i < objVal.Len(); i++ {
+			b := objVal.Index(i).Kind()
+			if b == reflect.Struct || b == reflect.Slice {
+				show(objVal.Index(i), objVal.Index(i).Type())
+			}
 		}
+		return
+	}
+	for i := 0; i < objVal.NumField(); i++ {
 		field := objVal.Field(i)
 		typeField := objType.Field(i)
 
 		fmt.Printf("%s: %v\n", typeField.Name, field.Interface())
+		b := field.Kind()
+		if b == reflect.Struct || b == reflect.Slice {
+			fmt.Printf("===== %s Desc =====\n", typeField.Name)
+			show(field, field.Type())
+			fmt.Println("==============================")
+			fmt.Println("\n")
+		}
+		// if field.NumField() > 1 {
+		// 	show(field, field.Type())
+		// }
 	}
-	fmt.Println("==============================")
 }
